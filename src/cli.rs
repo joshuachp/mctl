c17e29050865851a513e076a0078bdfb33945dde
Joshua Chapman <joshua.chapman@joshuachp.dev>
2025-02-06 13:51:06 +0100

    feat(cli): setup the cli commands

---
+++ src/cli.rs
@@ -0,0 +1,77 @@
+use std::{
+    io::{stdout, Write},
+    path::PathBuf,
+};
+
+use clap::{CommandFactory, Parser, Subcommand, ValueEnum};
+
+#[derive(Debug, Parser)]
+#[clap(version, about, bin_name = env!("CARGO_BIN_NAME"))]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    /// Utility functions like shell completions
+    Utils {
+        #[command(subcommand)]
+        command: Utils,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Utils {
+    /// Generates shell completions for the given shell
+    Completion {
+        /// Shell to generate the completion for
+        shell: Shell,
+    },
+    /// Generates the Man pages for the program
+    Manpage {
+        /// Directory to generate the Man pages to.
+        out_dir: PathBuf,
+    },
+}
+
+impl Utils {
+    pub(crate) fn run(&self) -> eyre::Result<()> {
+        match self {
+            Utils::Completion { shell } => shell.generate(),
+            Utils::Manpage { out_dir } => {
+                clap_mangen::generate_to(Cli::command(), out_dir)?;
+
+                Ok(())
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+pub enum Shell {
+    Bash,
+    Zsh,
+    Fish,
+}
+
+impl Shell {
+    fn generate(&self) -> eyre::Result<()> {
+        let gen = match self {
+            Shell::Bash => clap_complete::Shell::Bash,
+            Shell::Zsh => clap_complete::Shell::Zsh,
+            Shell::Fish => clap_complete::Shell::Fish,
+        };
+
+        let mut stdout = stdout().lock();
+        clap_complete::generate(
+            gen,
+            &mut Cli::command(),
+            env!("CARGO_BIN_NAME"),
+            &mut stdout,
+        );
+        stdout.flush()?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use cli::Cli;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+mod cli;
+
+fn main() -> eyre::Result<()> {
+    let cli = Cli::parse();
+
+    color_eyre::install()?;
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env()?,
+        )
+        .try_init()?;
+
+    match cli.command {
+        cli::Command::Utils { command } => {
+            command.run()?;
+        }
+    }
+
+    Ok(())
 }
